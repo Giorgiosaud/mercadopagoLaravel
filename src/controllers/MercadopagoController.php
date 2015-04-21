14c04f331a644fedeb522508e15bb1620d17453f
Jorge Saud <jorgelsaud@gmail.com>
2015-04-21 07:05:02 -0430

    namespace

---
+++ src/controllers/MercadopagoController.php
@@ -0,0 +1,19 @@
+<?php /**
+ * Created by jorgelsaud.
+ * User: jorgelsaud
+ * Date: 21/4/15
+ * Time: 7:01
+ */
+namespace jorgelsaud\Mercadopago\controllers;
+
+
+
+
+use Illuminate\Routing\Controller;
+
+class MercadopagoController extends Controller{
+    public function index(){
+        return 'hl';
+    }
+
+}
